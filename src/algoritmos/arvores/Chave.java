c4db93a730bf0b316b0bdd3a6465a8603d0a3bdb
raihlima <38303006+raihlima@users.noreply.github.com>
2018-10-24 14:56:10 -0300

    0.51
    Adicionando Nó das Aŕvores

---
+++ src/algoritmos/arvores/Chave.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmos.arvores;
+
+/**
+ *
+ * @author ice
+ */
+public class Chave {
+    private String data;
+    private String hora;
+    private int idDeputado;
+    private String partido;
+    private String estado;
+    private String deputado;
+    private long cnpj;
+    private String descricao;
+    private String empresa;
+    private float gasto;
+
+    public Chave() {
+    }
+
+    public Chave(String data, String hora, int idDeputado, String partido, String estado, String deputado, long cnpj, String descricao, String empresa, float gasto) {
+        this.data = data;
+        this.hora = hora;
+        this.idDeputado = idDeputado;
+        this.partido = partido;
+        this.estado = estado;
+        this.deputado = deputado;
+        this.cnpj = cnpj;
+        this.descricao = descricao;
+        this.empresa = empresa;
+        this.gasto = gasto;
+    }
+
+    public float getGasto() {
+        return gasto;
+    }
+
+    public void setGasto(float gasto) {
+        this.gasto = gasto;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public int getIdDeputado() {
+        return idDeputado;
+    }
+
+    public void setIdDeputado(int idDeputado) {
+        this.idDeputado = idDeputado;
+    }
+
+    public String getPartido() {
+        return partido;
+    }
+
+    public void setPartido(String partido) {
+        this.partido = partido;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getDeputado() {
+        return deputado;
+    }
+
+    public void setDeputado(String deputado) {
+        this.deputado = deputado;
+    }
+
+    public long getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(long cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+    
+    
+            
+}
---
+++ src/algoritmos/arvores/No.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmos.arvores;
+
+/**
+ *
+ * @author ice
+ */
+class No {
+    private int id;
+    private int altura;
+    private No dir;
+    private No esq;
+
+    public No() {
+        this.dir = null;
+        this.esq = null;
+    }
+
+    public No(int id) {
+        this.id = id;
+        this.dir = null;
+        this.esq = null;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public No getDir() {
+        return dir;
+    }
+
+    public void setDir(No dir) {
+        this.dir = dir;
+    }
+
+    public No getEsq() {
+        return esq;
+    }
+
+    public void setEsq(No esq) {
+        this.esq = esq;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+    
+    
+
+}
